ce6d66ff131ce38ea30eacc567ef8dc5ad586cc3
Om Pratap Singh <ompratap1999@gmail.com>
2024-09-02 11:22:40 +0530

    add_record template update

--- crm/website/views.py
+++ crm/website/views.py
@@ -46,14 +46,45 @@ def register_user(request):
     return render(request,'register.html',{'form':form})
         
 
-def customer_record(request):
-    return render(request,'register.html',{})
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request , 'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"YOu Must Be Logged IN")
+        return redirect('home')
 
-def delete_record(request):
-    return render(request,'register.html',{})
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,"Deleted successfully")
+    else:
+        messages.success(request,"Must be logged in")
+        return redirect('home')
 
 def add_record(request):
-    return render(request,'register.html',{})
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_vaild():
+                add_record = form.save()
+                messages.success(request,"Record Added...")
+                return redirect('home')
+        return render(request, 'add_record.html',{'form':form})
+    else:
+        messages.success(request,"You Must Be Logged In")
+        return redirect('home')
 
-def update_record(request):
-    return render(request,'register.html',{})
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None , instance=current_record)
+        if form.is_vaild():
+            form.save()
+            messages.success(request,"Record Has Been Update!")
+            return redirect('home')
+        return render(request,'update_record.html',{'form':form})
+    else:
+        messages.success(request,"Log In")
+        return redirect('home')
